1191717e74909e5b36662ecd988861098b7453fd
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-14 11:00:54 +0545

    Update word.js

--- word.js
+++ word.js
@@ -3,11 +3,11 @@
   "Success is not final; failure is not fatal.",
   "Dream big and dare to fail.",
   "You are stronger than you think.",
-  "The best way out is always through."
-  "You cannot change what you are, only what you do."
+  "The best way out is always through.",
+  "You cannot change what you are, only what you do.",
   "Every day is a new beginning.",
   "Happiness is not something ready-made; it comes from your actions.",
   "Hope is the most exciting thing there is in life.",
   "Say something positive, and you’ll see something positive.",
-  "Every strike brings me closer to the next home run.",
+  "Every strike brings me closer to the next home run."
 ]
