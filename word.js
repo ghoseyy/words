cba8e182f0e93570589791a234d8174ecad83e2c
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-14 10:50:08 +0545

    Create word.js

---
+++ word.js
@@ -0,0 +1,7 @@
+[
+  "Believe in yourself.",
+  "Success is not final; failure is not fatal.",
+  "Dream big and dare to fail.",
+  "You are stronger than you think.",
+  "The best way out is always through."
+]
