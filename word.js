5b58f09183ae6746e65d58230a91c33fcc957a8a
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-14 10:58:59 +0545

    Update word.js

--- word.js
+++ word.js
@@ -5,7 +5,9 @@
   "You are stronger than you think.",
   "The best way out is always through."
   "You cannot change what you are, only what you do."
-"Hope is the most exciting thing there is in life."
-"Say something positive, and you’ll see something positive."
-"Every strike brings me closer to the next home run."
+  "Every day is a new beginning.",
+  "Happiness is not something ready-made; it comes from your actions.",
+  "Hope is the most exciting thing there is in life.",
+  "Say something positive, and you’ll see something positive.",
+  "Every strike brings me closer to the next home run.",
 ]
