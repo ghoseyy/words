d016f5a3cb2fd7d0a9768511a611486e1da01561
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-14 10:56:27 +0545

    Update word.js

--- word.js
+++ word.js
@@ -4,4 +4,8 @@
   "Dream big and dare to fail.",
   "You are stronger than you think.",
   "The best way out is always through."
+  "You cannot change what you are, only what you do."
+"Hope is the most exciting thing there is in life."
+"Say something positive, and you’ll see something positive."
+"Every strike brings me closer to the next home run."
 ]
