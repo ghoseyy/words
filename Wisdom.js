82f3e8b62292aa7b3280894e3535ee4505a7771f
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-17 19:24:48 +0545

    Create Wisdom.js

---
+++ Wisdom.js
@@ -0,0 +1,14 @@
+[
+  "The only true wisdom is in knowing you know nothing.",
+  "Wisdom begins in wonder.",
+  "Knowledge speaks, but wisdom listens.",
+  "The wise man does at once what the fool does finally.",
+  "A fool thinks himself to be wise, but a wise man knows himself to be a fool.",
+  "Wisdom is not a product of schooling but of the lifelong attempt to acquire it.",
+  "The journey of a thousand miles begins with one step.",
+  "It is not length of life, but depth of life.",
+  "Wisdom is the reward you get for a lifetime of listening when you'd have preferred to talk.",
+  "To know, is to know that you know nothing. That is the meaning of true knowledge.",
+  "In seeking wisdom, we become aware of our own ignorance.",
+  "Patience is the companion of wisdom."
+]
