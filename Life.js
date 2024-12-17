77994c1e97ca37eec0780f31c025c8339948b81a
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-17 19:24:19 +0545

    Create Life.js

---
+++ Life.js
@@ -0,0 +1,14 @@
+[
+  "Life is what happens when you're busy making other plans.",
+  "The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.",
+  "In the end, we only regret the chances we didn't take.",
+  "Life isn’t about finding yourself. It’s about creating yourself.",
+  "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+  "Life is really simple, but we insist on making it complicated.",
+  "Don’t wait. The time will never be just right.",
+  "Live life as if everything is rigged in your favor.",
+  "Life is 10% what happens to us and 90% how we react to it.",
+  "Good friends, good books, and a sleepy conscience: this is the ideal life.",
+  "The best way to predict your future is to create it.",
+  "Life is short, and it's up to you to make it sweet."
+]
