98776e774e37087747128cac1ca657440d44d76d
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-17 19:23:40 +0545

    Create Motivation.js

---
+++ Motivation.js
@@ -0,0 +1,14 @@
+[
+  "The only limit to our realization of tomorrow is our doubts of today.",
+  "The future belongs to those who believe in the beauty of their dreams.",
+  "Success is not the key to happiness. Happiness is the key to success.",
+  "Don’t watch the clock; do what it does. Keep going.",
+  "Hardships often prepare ordinary people for an extraordinary destiny.",
+  "Your time is limited, so don’t waste it living someone else’s life.",
+  "Believe you can and you're halfway there.",
+  "Success is the sum of small efforts, repeated day in and day out.",
+  "The way to get started is to quit talking and begin doing.",
+  "It always seems impossible until it's done.",
+  "The only way to do great work is to love what you do.",
+  "Act as if what you do makes a difference. It does."
+]
