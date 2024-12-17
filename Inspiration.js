a43448ff752fd67d0d1c2d7fbe94d21f3e241430
Bimal chhetry <52375340+ghoseyy@users.noreply.github.com>
2024-12-17 19:09:26 +0545

    Create Inspiration.js

---
+++ Inspiration.js
@@ -0,0 +1,14 @@
+[
+  "Believe in yourself.",
+  "प्रत्येक क्षणलाई अपनाऊ, किनभने यसले तिम्रो भाग्यलाई आकार दिन्छ।"
+  "Success is not final; failure is not fatal.",
+  "Dream big and dare to fail.",
+  "You are stronger than you think.",
+  "The best way out is always through.",
+  "You cannot change what you are, only what you do.",
+  "Every day is a new beginning.",
+  "Happiness is not something ready-made; it comes from your actions.",
+  "Hope is the most exciting thing there is in life.",
+  "Say something positive, and you’ll see something positive.",
+  "Every strike brings me closer to the next home run."
+]
